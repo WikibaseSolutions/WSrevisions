a55bc9175ec7645791b41e79a3b2a9088e11b163
Charlot <charlot@wikibase>
2018-01-19 11:54:18 +0100

    Cody tidy up and create class for functions

--- WSrevisions.hooks.php
+++ WSrevisions.hooks.php
@@ -8,52 +8,34 @@
 
 class WSrevisionsHooks {
 
-	public static $db_prefix = "";
 
 	public function __construct() {
-		date_default_timezone_set('UTC');
-			global $wgDBprefix;
-			self::$db_prefix = $wgDBprefix;
+		date_default_timezone_set( 'UTC' );
 	}
 
 	public static function db_open() {
-			global $wgDBserver;
-			global $wgDBname;
-			global $wgDBuser;
-			global $wgDBpassword;
+		global $wgDBserver;
+		global $wgDBname;
+		global $wgDBuser;
+		global $wgDBpassword;
 
-			$conn = new MySQLi($wgDBserver, $wgDBuser, $wgDBpassword, $wgDBname);
-			$conn->set_charset("utf8");
-			return $conn;
+		$conn = new MySQLi( $wgDBserver, $wgDBuser, $wgDBpassword, $wgDBname );
+		$conn->set_charset( "utf8" );
+		return $conn;
 	}
 
-	public static function db_real_escape($txt) {
-			$db = WSssdHooks::db_open();
-			$txt = $db->real_escape_string($txt);
-			$db->close();
-			return $txt;
-
+	public static function db_real_escape( $txt ) {
+		$db = WSssdHooks::db_open();
+		$txt = $db->real_escape_string( $txt );
+		$db->close();
+		return $txt;
 	}
 
-	public static function rawSelect($sql) {
-			$dbr = wfGetDB(DB_SLAVE);
-			$dbr->IngoreErrors = true;
-			try {
-					$result = $dbr->query($sql, __METHOD__);
-					return $result;
-			} catch (Exception $e) {
-					echo "<pre>";
-					echo $e;
-					echo "</pre>";
-					return false;
-			}
-	}
-
-/**
- * When extension first called.. Setup hooks for magic words
- * @param  Parser $parser [description]
- * @return [type]         [description]
- */
+	/**
+	 * When extension first called.. Setup hooks for magic words
+	 * @param  Parser $parser [description]
+	 * @return [type]         [description]
+	 */
 	public static function onParserFirstCallInit( Parser &$parser ) {
 		$parser->setFunctionHook( 'ws_check_nme', 'WSrevisionsHooks::getRevisionsInfo' );
 		$parser->setFunctionHook( 'ws_size_diff', 'WSrevisionsHooks::getRevisionsSizeDiff' );
@@ -65,48 +47,34 @@ class WSrevisionsHooks {
 	 * @return [string]         [either ("Yes" or "No") or (0 or the amount of revisions)]
 	 */
 	public static function getRevisionsInfo( Parser &$parser ) {
-		$mysqldateformat = "YmdHis";
-		$options = WSrevisionsHooks::extractOptions( array_slice(func_get_args(), 1) );
 		global $wgDBprefix;
 
-		if (isset($options['pageid']) && $options['pageid'] != '' && is_numeric($options['pageid']) ) {
-			$pid = $options['pageid'];
-		} elseif( is_numeric(key($options)) ) {
-			$pid = key($options);
-		} else	return "No page ID";
+		$options 	= WSrevisionsHooks::extractOptions( array_slice(func_get_args(), 1) );
+		$pid 			= WSrevionsFunctions::getPageID( $options );
+		if(!$pid) return "No page ID";
+		$count 		= WSrevionsFunctions::getCount( $options );
+		$date 		= WSrevionsFunctions::getDate( $options );
+		$interval = WSrevionsFunctions::getInterval( $options );
+		$datum 		= WSrevionsFunctions::getPreviousDate( $interval, $date );
+		$date 		= WSrevionsFunctions::convertDateForSQL( $date );
 
-		if (isset($options['count']) ) {
-			$count=true;
-		} else $count=false;
 
-		if (isset($options['date']) && $options['date'] != '') {
-			$date = $options['date'];
-		} else {
-			$date = date($mysqldateformat);
-		}
-
-		if (isset($options['interval']) && $options['interval'] != '' && is_numeric($options['interval'])) {
-			$interval = $options['interval'];
-		} else $interval= 7;
-
-		$datum = date($mysqldateformat, strtotime("-".$interval." days", strtotime($date)));
-		$date = date($mysqldateformat, strtotime($date));
 		$sql = "SELECT rev_timestamp FROM ".$wgDBprefix."revision WHERE rev_minor_edit='0' AND rev_page='".$pid."' AND (rev_timestamp < '".$date."' AND rev_timestamp > '".$datum."')";
 		$db = self::db_open();
-		$q = $db->query($sql);
+		$q = $db->query( $sql );
 
-		if ($q->num_rows > 0 ) {
-			if($count) {
+		if ( $q->num_rows > 0 ) {
+			if( $count ) {
 				$lst = $q->num_rows;
 			} else $lst = "Yes";
     } else {
-			if($count) {
+			if( $count ) {
 				$lst=0;
-			}else $lst="No";
+			} else $lst="No";
 		}
 
     $db->close();
-		return array($lst, 'noparse' => false);
+		return array( $lst, 'noparse' => false );
 	  }
 
 
