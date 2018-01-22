d434be58efee1b69914857f8ae9839225c1f5dfe
Charlot <charlot@wikibase>
2018-01-22 09:59:25 +0100

    Added ws_size_diff

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Add the following line to your localsettings.php : <pre>require_once( "$IP/exten
 ### FAQ
 A major revision is a non-minor revision in this example.
 
+#### ws_check_nme
+
 ## Example 1
 Check pageid 231868 for major revisions in the last 7 days from now
 ```
@@ -29,3 +31,17 @@ Check if pageid 231868 had major revisions 7 days prior and up to 19-01-2018 00:
 {{#ws_check_nme: 231868|date=2018-01-19|interval=7|count}}
 ```
 Answer will be 0 for none or e.g. 14 if yes (so it will return the number of revisions)
+
+#### ws_size_diff
+
+## Example 1
+Get current page size in bytes from page with id 231868 and substract the major revision page size from 7 days ago (return size difference)
+```
+{{#ws_size_diff: 231868}}
+```
+
+## Example 2
+Get page size in bytes from page with id 231868 and revision date 2018-01-12 and substract the major revision page size from 14 days ago (return size difference)
+```
+{{#ws_size_diff: 231868|date=2018-01-12|interval=14}}
+```
--- WSrevisions.hooks.php
+++ WSrevisions.hooks.php
@@ -21,19 +21,23 @@ class WSrevisionsHooks {
 
 		$conn = new MySQLi( $wgDBserver, $wgDBuser, $wgDBpassword, $wgDBname );
 		$conn->set_charset( "utf8" );
+
 		return $conn;
 	}
 
 	public static function db_real_escape( $txt ) {
-		$db = WSssdHooks::db_open();
+		$db  = WSssdHooks::db_open();
 		$txt = $db->real_escape_string( $txt );
 		$db->close();
+
 		return $txt;
 	}
 
 	/**
 	 * When extension first called.. Setup hooks for magic words
+	 *
 	 * @param  Parser $parser [description]
+	 *
 	 * @return [type]         [description]
 	 */
 	public static function onParserFirstCallInit( Parser &$parser ) {
@@ -43,42 +47,92 @@ class WSrevisionsHooks {
 
 	/**
 	 * [WSrevision have there been Major revisions in th last x days starting from date ...]
+	 *
 	 * @param  Parser $parser [description]
+	 *
 	 * @return [string]         [either ("Yes" or "No") or (0 or the amount of revisions)]
 	 */
 	public static function getRevisionsInfo( Parser &$parser ) {
 		global $wgDBprefix;
 
-		$options 	= WSrevisionsHooks::extractOptions( array_slice(func_get_args(), 1) );
-		$pid 			= WSrevionsFunctions::getPageID( $options );
-		if(!$pid) return "No page ID";
-		$count 		= WSrevionsFunctions::getCount( $options );
-		$date 		= WSrevionsFunctions::getDate( $options );
+		$options = WSrevisionsHooks::extractOptions( array_slice( func_get_args(), 1 ) );
+		$pid     = WSrevionsFunctions::getPageID( $options );
+		if ( ! $pid ) {
+			return "No page ID";
+		}
+		$count    = WSrevionsFunctions::getCount( $options );
+		$date     = WSrevionsFunctions::getDate( $options );
+		if( !$date ) return "invalid date";
 		$interval = WSrevionsFunctions::getInterval( $options );
-		$datum 		= WSrevionsFunctions::getPreviousDate( $interval, $date );
-		$date 		= WSrevionsFunctions::convertDateForSQL( $date );
+		$datum    = WSrevionsFunctions::getPreviousDate( $interval, $date );
+		$date     = WSrevionsFunctions::convertDateForSQL( $date );
 
 
-		$sql = "SELECT rev_timestamp FROM ".$wgDBprefix."revision WHERE rev_minor_edit='0' AND rev_page='".$pid."' AND (rev_timestamp < '".$date."' AND rev_timestamp > '".$datum."')";
-		$db = self::db_open();
-		$q = $db->query( $sql );
+		$sql = "SELECT rev_timestamp FROM " . $wgDBprefix . "revision WHERE rev_minor_edit='0' AND rev_page='" . $pid . "' AND (rev_timestamp < '" . $date . "' AND rev_timestamp > '" . $datum . "')";
+
+		$db  = self::db_open();
+		$q   = $db->query( $sql );
 
 		if ( $q->num_rows > 0 ) {
-			if( $count ) {
+			if ( $count ) {
 				$lst = $q->num_rows;
-			} else $lst = "Yes";
-    } else {
-			if( $count ) {
-				$lst=0;
-			} else $lst="No";
+			} else {
+				$lst = "Yes";
+			}
+		} else {
+			if ( $count ) {
+				$lst = 0;
+			} else {
+				$lst = "No";
+			}
 		}
 
-    $db->close();
+		$db->close();
+
 		return array( $lst, 'noparse' => false );
-	  }
+	}
 
-
+	/**
+	 * Get size difference between revisions
+	 * @param Parser $parser
+	 *
+	 * @return int size in bytes
+	 */
 	public static function getRevisionsSizeDiff( Parser &$parser ) {
+		global $wgDBprefix;
+
+		$options = WSrevisionsHooks::extractOptions( array_slice( func_get_args(), 1 ) );
+		$pid     = WSrevionsFunctions::getPageID( $options );
+		if ( ! $pid ) {
+			return "No page ID";
+		}
+		$date     = WSrevionsFunctions::getDate( $options );
+		if( !$date ) return "invalid date";
+		$interval = WSrevionsFunctions::getInterval( $options );
+		$datum    = WSrevionsFunctions::getPreviousDate( $interval, $date );
+		$date     = WSrevionsFunctions::convertDateForSQL( $date );
+
+
+		$sql = "SELECT rev_len FROM " . $wgDBprefix . "revision WHERE rev_minor_edit='0' AND rev_page='" . $pid . "' AND (rev_timestamp <= '".$date."') ORDER BY rev_timestamp DESC LIMIT 1";
+		$db  = self::db_open();
+		$q   = $db->query( $sql );
+
+		if ( $q->num_rows > 0 ) {
+			$row = $q->fetch_assoc();
+			$length_current_page = $row['rev_len'];
+		} else return "No current revision for this page";
+
+		$sql = "SELECT rev_len FROM " . $wgDBprefix . "revision WHERE rev_minor_edit='0' AND rev_page='" . $pid . "' AND (rev_timestamp < '".$datum."') ORDER BY rev_timestamp DESC LIMIT 1";
+		$q   = $db->query( $sql );
+
+		if ( $q->num_rows > 0 ) {
+			$row = $q->fetch_assoc();
+			$length_previous_version = $row['rev_len'];
+		} else return "0";
+		$db->close();
+		$ret = $length_current_page - $length_previous_version;
+
+		return array( $ret, 'noparse' => false );
 
 	}
 
@@ -88,6 +142,7 @@ class WSrevisionsHooks {
 	 * true is assumed like this: [name] => true
 	 *
 	 * @param array string $options
+	 *
 	 * @return array $results
 	 */
 	public static function extractOptions( array $options ) {
@@ -95,13 +150,13 @@ class WSrevisionsHooks {
 		foreach ( $options as $option ) {
 			$pair = explode( '=', $option, 2 );
 			if ( count( $pair ) === 2 ) {
-				$name = trim( $pair[0] );
-				$value = trim( $pair[1] );
-				$results[$name] = $value;
+				$name             = trim( $pair[0] );
+				$value            = trim( $pair[1] );
+				$results[ $name ] = $value;
 			}
 			if ( count( $pair ) === 1 ) {
-				$name = trim( $pair[0] );
-				$results[$name] = true;
+				$name             = trim( $pair[0] );
+				$results[ $name ] = true;
 			}
 		}
 		//Now you've got an array that looks like this:
